5a83dbc004f4a460be1464dcfa4ee76b428b0f4a
starcraft168 <ericeng168@gmail.com>
2017-01-29 11:55:01 -0800

    add node_modules

---
+++ main.js
@@ -0,0 +1,5 @@
+var natural = require('natural');
+var myString = "I'm surprised! I didn't know you could make it.";
+var tokenizer = new natural.WordTokenizer();
+
+console.log(tokenizer.tokenize(myString));
