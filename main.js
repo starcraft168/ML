bad6a57cc389c79154bbc23ef0e3614dd28e10bf
starcraft168 <ericeng168@gmail.com>
2017-01-29 13:04:34 -0800

    commit

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 var natural = require('natural');
 var myString = "I'm surprised! I didn't know you could make it.";
-var tokenizer = new natural.WordTokenizer();
+var tokenizer = new natural.RegexpTokenizer({patter});
 
 console.log(tokenizer.tokenize(myString));
+
+stemming means to find the roots of words
+var natural = require('natural');
+var stemmer = natural.PorterStemmer;
+
+console.log(stemmer.stem("mangoes"));
+console.log(stemmer.stem("showing"));
+
+var nounInflector = new natural.NounInflector();
+console.log(nounInflector.pluralize("mouse"));
+console.log(nounInflector.singularize("tomatoes"));
+
+var countInflector = natural.CountInflector;
+
+for(var i=0; i<10; i++) {
+	console.log(countInflector.nth(i));
+}
+
+var NGrams = natural.NGrams;
+
+var myString = "whats up man you are cool Jane Smith man Max";
+console.log(NGrams.bigrams(myString));
+helps find all the names in the sentence
+
+var Tagger = natural.BrillPOSTagger;
+var myString = "Lys soldered the beautiful jewelry pieces";
+
